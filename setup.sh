138d14feea077f5a069c14eebd21c1c85e1d1337
Joey <joeymiscellaneous@gmail.com>
2026-03-22 14:42:11 -0400

    Fixed composer installation in setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,6 @@ docker compose build
 docker compose run dev-php composer install
 docker compose run dev-vue3 npm install
 
-docker compose up -d
+docker compose up -d --remove-orphans
 
 echo "Navigate to http://localhost:3000"
